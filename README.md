72049615ade2fbf7d865ec048cbbea0b0e3a0b75
Armaan Shah <armaanshah96@gmail.com>
2016-12-21 11:44:32 -0500

    added slack button authorization reqeusts

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# cuRate2
+#slashCurate
+####**
+#####*21 Dec 2016*
+
+***
+####Overview
+cuRate is a Slack bot that automatically curates your Twitter feed. 
+
+Get the relevant info for your team sent straight to the channel of your choice! 
+
+cuRate reads the desired users' tweets from the past day, and scores each tweet based on how many Twitter users favorited and retweeted. Only the best tweet from each account will be sent to the Slack channel. 
--- app.js
+++ app.js
@@ -13,6 +13,28 @@ app.listen(process.env.PORT || 3000,function() {
 	console.log('curate listening on port 3000');
 });
 
+app.get('/authorize', function(req, res){
+	var data = {
+		form: {
+			client_id: process.env.SLACK_CLIENT_ID,
+			client_secret: process.env.SLACK_CLIENT_SECRET,
+			code: req.query.code
+		}
+	};
+	request.post('https://slack.com/api/oauth.access', data, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			// If you want to get team info, you need to get the token here
+			let token = JSON.parse(body).access_token; // Auth token
+		}
+	});
+	request.post('https://slack.com/api/team.info', {form: {token: token}},function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			let team = JSON.parse(body).team.domain;
+			res.redirect('http://' +team+ '.slack.com');
+		}
+	});
+}
+
 app.post('/', function(req,res) {
 	// ensure that this command came from slack, and if not don't do anything
 	if(process.env.SLACK_TOKEN !== req.body.token) return;
@@ -87,8 +109,6 @@ app.post('/', function(req,res) {
 			// add attachment to message array
 			message.attachments.push(attachment);
 		});
-
-		console.log('MESSAGE is CREATED', message); //******
 
 		// prepare post request fields
 		var options = {
